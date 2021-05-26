17c9cc2bd6bd9047948d0d263e3d5d804fd3bb58
Clemens Schueller <schueller.cj@gmail.com>
2021-05-26 12:50:31 +0200

    Solution multiple answer seperated by commas

--- index.js
+++ index.js
@@ -6,7 +6,14 @@ const rl = readline.createInterface({
   output: process.stdout,
 })
 
-rl.question('Hello, how are you?', answer => {
-  console.log(answer)
-  rl.close()
-})
+rl.question(
+  'Hello, which functions and files would you like to create? Please seperate by comma.',
+  answer => {
+    answer
+      .split(',')
+      .map(wurst => wurst.trim())
+      .forEach(wurst => writeFile(wurst))
+    // Lene's Weg: answer.split(',').map(name => writeFile(name.trim()))
+    rl.close()
+  }
+)
